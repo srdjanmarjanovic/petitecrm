5be4bc3c32412c9b642e0ff7975d24b6d5846561
Srdjan Marjanovic <srdjan.marjan@gmail.com>
2016-07-23 12:18:11 +0200

    Introduce Companies, Contacs, Lists, Tags routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,3 +18,47 @@ Route::get('/', function () {
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
+
+Route::resource('contacts', 'ContactController', [
+    'names' => [
+        'create' => 'contact.create',
+        'show' => 'contact.single',
+        'index' => 'contacts.all',
+        'update' => 'contact.update',
+        'edit' => 'contact.edit',
+        'store' => 'contact.save',
+    ]
+]);
+
+Route::resource('companies', 'CompanyController', [
+    'names' => [
+        'create' => 'company.create',
+        'show' => 'company.single',
+        'index' => 'companies.all',
+        'update' => 'company.update',
+        'edit' => 'company.edit',
+        'store' => 'company.save',
+    ]
+]);
+
+Route::resource('lists', 'ListController', [
+    'names' => [
+        'create' => 'list.create',
+        'show' => 'list.single',
+        'index' => 'lists.all',
+        'update' => 'list.update',
+        'edit' => 'list.edit',
+        'store' => 'list.save',
+    ]
+]);
+
+Route::resource('tags', 'TagsController', [
+    'names' => [
+        'create' => 'tag.create',
+        'show' => 'tag.single',
+        'index' => 'tags.all',
+        'update' => 'tag.update',
+        'edit' => 'tag.edit',
+        'store' => 'tag.save',
+    ]
+]);
