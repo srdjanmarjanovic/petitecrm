2b00d54571181e6f119bfd3063ac3e128e6ac835
Srdjan Marjanovic <srdjan.marjan@gmail.com>
2016-08-10 18:36:30 +0200

    Minor changes to list contact view

--- resources/views/contacts/partials/list.blade.php
+++ resources/views/contacts/partials/list.blade.php
@@ -15,7 +15,8 @@
                     <!-- Check all button -->
                     <button type="button" class="btn btn-flat btn-default btn-xs checkbox-toggle"><i class="fa fa-square-o"></i></button>
                     <div class="pull-right">
-                        {!! $contacts->firstItem() .' - '. $contacts->lastItem() !!}/{!! $contacts->total() !!} results
+                        <small class="text-muted">{!! $contacts->firstItem() .' - '. $contacts->lastItem() !!}/{!! $contacts->total() !!} results</small>
+
                         @if ($contacts->hasMorePages() || $contacts->previousPageUrl())
                             @include('contacts.partials.pagination.btns')
                         @endif
@@ -35,22 +36,29 @@
                   <tr>
                     <td class="mailbox-star">
                         <input type="checkbox" value="{{ $contact->id }}" name="bulkcontacts[]"><span>&nbsp;</span>
-                        <i class="fa {{ $contact->getTypeClass() }} text-yellow"></i>
                     </td>
                     {{--<td><div aria-disabled="false" aria-checked="false" style="position: relative;" class="icheckbox_flat-blue"><input style="position: absolute; opacity: 0;" type="checkbox"><ins style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255) none repeat scroll 0% 0%; border: 0px none; opacity: 0;" class="iCheck-helper"></ins></div></td>--}}
                     {{--<td class="mailbox-star" title="{{ $contact->type }}"><i class="fa {{ $contact->getTypeClass() }} text-yellow"></i></td>--}}
                     <td class="mailbox-name">
-                        <a href="{{ route('contact.single', ['id' => $contact->id]) }}">
+                        <i class="fa {{ $contact->getTypeClass() }} text-yellow" title="{{ ucfirst($contact->type) }}"></i>
+                        <a class="lead" href="{{ route('contact.single', ['id' => $contact->id]) }}">
                             {{ $contact->getDisplayName() }}
                         </a>
+
+                         @if($contact->getRoleInCompany())
+                            <div>
+                                  <small class="text-muted">
+                                     {{ $contact->getRoleInCompany() }}
+                                  </small>
+                            </div>
+                         @endif
+
                     </td>
                     <td class="mailbox-subject">
                         {{ $contact->email }}
                     </td>
                     <td class="mailbox-subject">
-                        @if($contact->company)
-                            {{ $contact->company->name }}
-                        @endif
+
                     </td>
                     <td class="mailbox-date text-muted text-right" title="{{ $contact->created_at }}"><small>{{ $contact->created_at->diffForHumans() }}</small></td>
                     <td>
@@ -92,7 +100,7 @@
                     <button type="button" class="btn btn-default btn-xs"><i class="fa fa-refresh"></i></button>
                     <!-- /.btn-group -->
                     <div class="pull-right">
-                        {!! $contacts->firstItem() .' - '. $contacts->lastItem() !!}/{!! $contacts->total() !!} results
+                        <small class="text-muted">{!! $contacts->firstItem() .' - '. $contacts->lastItem() !!}/{!! $contacts->total() !!} results</small>
                         @if ($contacts->hasMorePages() || $contacts->previousPageUrl())
                             @include('contacts.partials.pagination.btns')
                         @endif
