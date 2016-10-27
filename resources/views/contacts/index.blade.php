c0192eb95dfa17842e5cd4c56be1206945b0e54e
Srdjan Marjanovic <srdjan.marjan@gmail.com>
2016-10-27 22:39:26 +0200

    Move quick list to section one menu
    https://projects.zoho.com/portal/srdjan\#taskdetail/1005713000000014343/1005713000000014485/1005713000000014469

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -5,6 +5,23 @@
 
 @section('section-one')
 	<ul class="contacts-top-menu list-inline text-right margin-tb-10">
+        <li>
+            <div class="dropdown">
+              <a class="dropdown-toggle"id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                <span class="caret"></span>
+                Quick List
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                <li><a href="#">Leads</a></li>
+                <li><a href="#">Prospects</a></li>
+                <li><a href="#">Customers</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#">Recently created</a></li>
+                <li><a href="#">Recently contacted</a></li>
+                <li><a href="#">Never contacted</a></li>
+              </ul>
+            </div>
+        </li>
 	    <li><a href="#">Advanced search</a></li>
 	    <li><a href="{{ route('contacts.import.form') }}">Import</a></li>
 	    <li><a href="#">Detect duplicates</a></li>
