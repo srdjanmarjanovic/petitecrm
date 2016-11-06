ffd6d3b7f2083ae6544ecf55996ea24afd3b46c7
Srdjan Marjanovic <srdjan.marjan@gmail.com>
2016-11-06 01:23:45 +0100

    Remove collapse button for sidebar filers
    https://projects.zoho.com/portal/srdjan\#taskdetail/1005713000000014343/1005713000000014359/1005713000000014453

--- resources/views/tags/partials/sidebar-block.blade.php
+++ resources/views/tags/partials/sidebar-block.blade.php
@@ -1,9 +1,6 @@
 <div class="box box-solid sidebar-block">
     <div class="box-header with-border">
         <h3 class="box-title">Filter by tag</h3>
-        <div class="box-tools">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
-        </div>
     </div>
     <div class="box-body no-padding">
         <ul class="nav nav-pills nav-stacked">
